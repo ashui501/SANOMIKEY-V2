5d00a4e9f4bf592981eeea5dae7d5d6cdcd10054
darkmadara09 <136217534+darkmadara09@users.noreply.github.com>
2024-01-11 16:35:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,12 +2,12 @@ import os
 from os import getenv
 
 
-API_ID = int(getenv("API_ID", "6435225"))
-API_HASH = getenv("API_HASH", "4e984ea35f854762dcde906dce426c2d")
-BOT_USERNAME = getenv("BOT_USERNAME", "YumikooBot")
+API_ID = int(getenv("API_ID", "27205215"))
+API_HASH = getenv("API_HASH", "105e1f779e13d401ff0131bc88ed5063")
+BOT_USERNAME = getenv("BOT_USERNAME", "Dazai_ixbot")
 COMMAND_HANDLER = ["/", "!"]
-BOT_TOKEN = getenv("BOT_TOKEN", "6743158347:AAG0lotMQpA7jGYaFmQN5DtIuNoJDxzA-yo")
-OWNER_ID = int(getenv("OWNER_ID", "6691393517"))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6691393517").split()))
+BOT_TOKEN = getenv("BOT_TOKEN", "6964505365:AAFzNSGY1ReQ2B68zjLzb3AAZAneVm7Epok")
+OWNER_ID = int(getenv("OWNER_ID", "5054912509"))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6758236533").split()))
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority")
-SESSION_STRING = getenv("SESSION_STRING", "BQFYNvIAZ1yg6dKk5_rSeO3UAhVcMPij6AFkjVRk-4-lqeIzcHoO7QlXMBbBDtu-8ydfKJGTGQpgy3BbClq7-T_rRZbUJfPULWUpLuTdF4ljlvIWKHMw6VyVEuzRf_6JJGVEyN0fAMIrSmdwiKuZ1zUx9HpVqPw6")
+SESSION_STRING = getenv("SESSION_STRING", "BQCVPypGMadCL97iCWzxOa2PqkVF2kURgZ-XDdCfwKaU2zGK945kaf6_7DzeX8ZKmrbPSgLBZ1pGqTy_TcwamRHZD475CSE8EdoO9pcoWZXM4kIgVJVYU9j_Nb4ijFof2fHAeF71MxQtRroSVqj_fQWemuu1sP_nzo-NCUjGiyiF-I_ceDgNNjLsmQEgH8sKa2XQoWlxGFLb2UkkhYOk3jpOn6EzSq3bGVje3iKjSwhhpDcseVDIEB4z7uMwcGBYWImKYOWi01pDYQslQjrWb2Xmh8EKsYbFQeXUtVIweDK8oVpWFwUsgxE_afstk15eqzot88gkIRnN_vlvQTIbrEzZAAAAAYYwpwkA")
